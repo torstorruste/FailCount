4876e6ddb04329da814a601b01b4a5d005a353b9
Tor Storruste <tor@superhelt.org>
2014-03-02 15:28:03 +0100

    Only using lua, added event handling

--- FailCount.lua
+++ FailCount.lua
@@ -1,9 +1,29 @@
+local frame = CreateFrame("FRAME", "FailCountFrame");
+frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
+frame:RegisterEvent("PLAYER_ENTERING_WORLD");
+frame:RegisterEvent("PLAYER_REGEN_DISABLED");
+frame:RegisterEvent("PLAYER_REGEN_ENABLED");
+
 SLASH_FAILCOUNT1 = '/fc';
 
-function FailCount_OnLoad()
-	print("FailCount initiating");
-end
+fails = {};
+currentEncounter = nil;
+
+function FailCount_OnEvent(self, event, ...)
+	print("FailCount: " .. event);
+end;
+frame:SetScript("OnEvent", FailCount_OnEvent);
 
 function SlashCmdList.FAILCOUNT(msg, editbox)
-	print("FAILSLASH " .. msg);
+	print("Fails " .. msg);
+	for k, v in pairs(fails) do print(k,v); end
+end;
+
+function FailCount_AddFail(player, ability)
+	if fails[msg] ~= nil then
+		fails[msg] = fails[msg] + 1;
+	else
+		fails[msg] = 1;
+	end
+	print(fails[msg]);
 end;
